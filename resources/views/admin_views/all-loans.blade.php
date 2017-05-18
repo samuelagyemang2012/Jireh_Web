e4b06bde0ca279c8767225a8a49fcd449cf383c9
Samuel Agyemang <khermz2012@gmail.com>
2017-05-18 17:34:01 +0100

    workin on login

---
+++ resources/views/admin_views/all-loans.blade.php
@@ -0,0 +1,54 @@
+@extends('master3')
+
+@section('content')
+    <div class="box-footer clearfix">
+
+        <table class="table">
+            <thead>
+            <tr>
+                <th>Surname</th>
+                <th>Firstname</th>
+                <th>Email</th>
+                <th>Telephone (Mobile)</th>
+                <th>Amount Requested</th>
+                <th></th>
+                <th></th>
+                {{--<th></th>--}}
+            </tr>
+            </thead>
+            <tbody>
+
+            @foreach($all as $a)
+                <tr>
+                    <form method="post" action="{{route('details')}}">
+                        {{csrf_field()}}
+                        <td>{{$a->surname}}</td>
+                        <td>{{$a->firstname}}</td>
+                        <td>{{$a->email}}</td>
+                        <td>{{$a->telephone_mobile}}</td>
+                        <td><span>GHC </span>{{$a->amount_requested}}</td>
+                        <td><input name="email" value="{{$a->email}}" hidden></td>
+                        <td><input name="id" value="{{$a->id}}" hidden></td>
+                        <td>
+                            <button type="submit" class="btn btn-sm btn-primary">More Details</button>
+                        </td>
+                    </form>
+                </tr>
+            @endforeach
+
+            </tbody>
+        </table>
+
+        <table>
+            <thead>
+            <th>
+                <button class="btn btn-sm btn-primary">Print</button>&nbsp;
+            </th>
+            <th>
+                <button class="btn btn-sm btn-primary">Download</button>
+            </th>
+            </thead>
+        </table>
+    </div>
+    {{--<hr>--}}
+@stop
