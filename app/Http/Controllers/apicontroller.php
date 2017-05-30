09170d6b48d32cbddfc94c8e5324e72199abd4bf
samuelagyemang2012 <khermz2012@gmail.com>
2017-05-30 19:09:39 +0100

    testing apis

--- app/Http/Controllers/apicontroller.php
+++ app/Http/Controllers/apicontroller.php
@@ -9,25 +9,24 @@ class apicontroller extends Controller
 {
     public function login($username, $password)
     {
-//        echo "sdad";
-//        $u = new User;
-//
-//        $response = $u->login($username, $password);
-//
-//        if ($response == 1) {
-//
+        $u = new User;
+
+        $response = $u->login($username, $password);
+
+        if ($response == 1) {
+
             return response()->json([
                 'code' => 1,
                 'msg' => "Login Successful"
             ]);
-//
-//        } else {
-//
-//            return response()->json([
-//                'code' => 0,
-//                'msg' => "Login Successful"
-//            ]);
-//        }
+
+        } else {
+
+            return response()->json([
+                'code' => 0,
+                'msg' => "Login Successful"
+            ]);
+        }
     }
 }
 
--- routes/web.php
+++ routes/web.php
@@ -71,6 +71,6 @@ Route::get('test', 'admincontroller@test')->name('test');
 
 //APIs
 
-Route::get('api-login/{email}/{password}', 'apicontroller@login')->name('api-login');
+Route::post('api-login/{email}/{password}', 'apicontroller@login')->name('api-login');
 
 
