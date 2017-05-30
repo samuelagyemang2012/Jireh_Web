641d2b8d62f3e3df5149cdfc300fbcbce89ad6aa
samuelagyemang2012 <khermz2012@gmail.com>
2017-05-30 19:59:04 +0100

    testing apis

--- app/Http/Controllers/apicontroller.php
+++ app/Http/Controllers/apicontroller.php
@@ -3,18 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use Illuminate\Http\Request;
 
 
 class apicontroller extends Controller
 {
-    public function login($email, $password)
+    public function login(Request $request)
     {
         $u = new User;
 
+        $input = $request->all();
+
+        return $input;
+
 //        $response = $u->login($email, $password);
-
-        Auth::guard('api')->user();
-
+//
+////        Auth::guard('api')->user();
+//
 //        if ($response == 1) {
 //
 //            $token = str_random(60);
