8d80fb35666b75db2ff39e23d5cb58497d103e02
samuelagyemang2012 <khermz2012@gmail.com>
2017-05-30 20:10:34 +0100

    testing apis

--- app/Http/Controllers/apicontroller.php
+++ app/Http/Controllers/apicontroller.php
@@ -8,36 +8,15 @@ use Illuminate\Http\Request;
 
 class apicontroller extends Controller
 {
-    public function login(Request $request)
+    public function login($email, $password)
     {
-        $u = new User;
 
-        $input = $request->all();
+        $email = $_POST['email'];
+        $password = $_POST['password'];
 
-        return $input;
-
-//        $response = $u->login($email, $password);
-//
-////        Auth::guard('api')->user();
-//
-//        if ($response == 1) {
-//
-//            $token = str_random(60);
-//
-//            return response()->json([
-//                'code' => 1,
-//                'msg' => "Login Successful",
-//                'token' => $token
-//            ]);
-//
-//        } else {
-//
-//            return response()->json([
-//                'code' => 0,
-//                'msg' => "Login Successful",
-//                'token' => null
-//            ]);
-//        }
+        return $email;
     }
+
+
 }
 
