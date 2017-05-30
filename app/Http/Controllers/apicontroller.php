38e181bddb4803e5438076be92c65966b09a9d65
Samuel Agyemang <khermz2012@gmail.com>
2017-05-30 18:16:42 +0100

    api test

---
+++ app/Http/Controllers/apicontroller.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+
+
+class apicontroller extends Controller
+{
+    public function login($username, $password)
+    {
+        $u = new User;
+
+        $response = $u->login($username, $password);
+
+        if ($response == 1) {
+
+            return response()->json([
+                'code' => 1,
+                'msg' => "Login Successful"
+            ]);
+
+        } else {
+
+            return response()->json([
+                'code' => 0,
+                'msg' => "Login Successful"
+            ]);
+        }
+    }
+}
+
