1b1ae079b18a055a09468262428b8489bce75005
samuelagyemang2012 <khermz2012@gmail.com>
2017-05-31 13:41:45 +0100

    testing apis

--- app/Http/Controllers/apicontroller.php
+++ app/Http/Controllers/apicontroller.php
@@ -12,6 +12,7 @@ use App\Spouse;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Mail;
 
 class apicontroller extends Controller
 {
@@ -185,23 +186,12 @@ class apicontroller extends Controller
                 $npass = bcrypt($inputs['password']);
 
                 $today = date("l jS \of F Y h:i:s A");
+
                 $user->insert($inputs['surname'], $inputs['firstname'], $inputs['othernames'], $npass, $inputs['email'], "test", $today);
 //                $spouse->insert($inputs['email'], $inputs['spousename'], $inputs['saddress'], $inputs['stel']);
 //                $employer->insert($inputs['email'], $inputs['employer_name'], $inputs['employer_address']);
-//
 //                $client->insert($inputs['email'], $inputs['title'], $inputs['gender'], $inputs['num_children'], $inputs['residential_address'], $inputs['mailing_address'], $inputs['telephone_mobile'], $inputs['telephone_official'], $inputs['date_of_birth'], $inputs['occupation'], $inputs['position'], $inputs['nationality'], $inputs['numyears'], $inputs['marital_status'], $inputs['source_of_funds'], $inputs['monthly_income'], $inputs['identification'], $inputs['identification_number'], $inputs['issuedate'], $inputs['expirydate'], $inputs['literacy'], $inputs['hometown'], $inputs['social_security'], $inputs['numhousehold'], $inputs['numdependants'], $inputs['father'], $inputs['mother'], $inputs['kname'], $inputs['kaddress'], $inputs['ktel'], $inputs['krel']);
 
-//
-////            if (Input::hasFile('pic')) {
-////                $file = Input::file('pic');
-////                $file->move('uploads', $file->getClientOriginalName());
-////            }
-//
-////            return response()->json([
-////                "code" => 0
-////            ]);
-////
-////
                 $fname = $inputs['firstname'];
                 $sname = $inputs['surname'];
 //
@@ -234,5 +224,14 @@ class apicontroller extends Controller
 
             }
         }
+    }
+
+    private function mail($data, $email, $subject)
+    {
+        Mail::send('email_views.email', $data, function ($m) use ($email, $subject) {
+            $m->from('info@jirehmfl.com.gh', 'Jireh Microfinance Ltd');
+            $m->to($email);
+            $m->subject($subject);
+        });
     }
 }
