f7de35fda4c79d2e5212f1e3a56e3646499cf787
samuelagyemang2012 <khermz2012@gmail.com>
2017-05-31 16:10:53 +0100

    testing apis

--- app/Http/Controllers/apicontroller.php
+++ app/Http/Controllers/apicontroller.php
@@ -266,61 +266,61 @@ class apicontroller extends Controller
         $date = date("l jS \of F Y h:i:s A");
         $email = Session::get('memail');
 
-        $rules = [
-            'num_monthly' => 'required|min:1',
-            'bank' => 'required|min:2',
-            'salary_date' => 'required',
-            'numloans' => 'required|min:0',
-            'total_monthly_payments' => 'required|min:0',
-            'name_insti' => 'required|min:2',
-            'amount' => 'required|min:1',
-            'loan_period' => 'required|min:1',
-            'purpose' => 'required|min:5',
-            'collateral' => 'required|min:5',
-            'wname' => 'required|min:2',
-            'wemployer' => 'required|min:2',
-            'wtel' => 'required|min:10'
-        ];
-
-        $messages = [
-            'num_monthly.required' => 'The Total Monthly Payments field is required',
-            'num_monthly.min' => 'The Total Monthly Payments field must be at least 1',
-
-            'bank.required' => 'The Bank field is required',
-            'bank.min' => 'The Bank field must be at least 2 characters',
-
-            'salary_date.required' => 'The Salary Date field is required',
-
-            'numloans.required' => 'The Number of Current Loans field is required',
-            'numloans.min' => 'The Number of Current Loans field cannot be negative',
-
-            'total_monthly_payments.required' => 'The Total Monthly Payments field is required',
-            'total_montly_payment.min' => 'The Total Monthly Payments field cannot be negative',
-
-
-            'name_insti.required' => 'The Name of Institution field is required',
-            'name_insti.min' => 'The Name ofInstitution field must be at least 2 characters',
-
-            'amount.required' => 'The Amount field is required',
-            'amount.min' => 'The amount requested field cannot ne negative',
-
-            'loan_period.required' => 'The Proposed Loan Period field is required',
-            'loan_period.min' => 'The Proposed Loan Period field must be at least 2 characters',
-
-            'collateral.required' => 'The Collateral Details field is required',
-            'collateral.min' => 'The Collateral Details field must be at least 5 charaters',
-
-            'wname.required' => 'The Witness\' Name field is required',
-            'wname.min' => 'The Witness\' Name field must be at least 2 characters',
-
-            'wemployer.required' => 'The Witness\' Employer field is required',
-            'wemployer.min' => 'The Witness\' Employer field must be at least 2 character',
-
-            'wtel.required' => 'The Witness\' Telephone field is required',
-            'wtel.min' => 'The Witness\' Telephone must be at least 10 characters'
-        ];
-
-        $this->validate($request, $rules, $messages);
+//        $rules = [
+//            'num_monthly' => 'required|min:1',
+//            'bank' => 'required|min:2',
+//            'salary_date' => 'required',
+//            'numloans' => 'required|min:0',
+//            'total_monthly_payments' => 'required|min:0',
+//            'name_insti' => 'required|min:2',
+//            'amount' => 'required|min:1',
+//            'loan_period' => 'required|min:1',
+//            'purpose' => 'required|min:5',
+//            'collateral' => 'required|min:5',
+//            'wname' => 'required|min:2',
+//            'wemployer' => 'required|min:2',
+//            'wtel' => 'required|min:10'
+//        ];
+//
+//        $messages = [
+//            'num_monthly.required' => 'The Total Monthly Payments field is required',
+//            'num_monthly.min' => 'The Total Monthly Payments field must be at least 1',
+//
+//            'bank.required' => 'The Bank field is required',
+//            'bank.min' => 'The Bank field must be at least 2 characters',
+//
+//            'salary_date.required' => 'The Salary Date field is required',
+//
+//            'numloans.required' => 'The Number of Current Loans field is required',
+//            'numloans.min' => 'The Number of Current Loans field cannot be negative',
+//
+//            'total_monthly_payments.required' => 'The Total Monthly Payments field is required',
+//            'total_montly_payment.min' => 'The Total Monthly Payments field cannot be negative',
+//
+//
+//            'name_insti.required' => 'The Name of Institution field is required',
+//            'name_insti.min' => 'The Name ofInstitution field must be at least 2 characters',
+//
+//            'amount.required' => 'The Amount field is required',
+//            'amount.min' => 'The amount requested field cannot ne negative',
+//
+//            'loan_period.required' => 'The Proposed Loan Period field is required',
+//            'loan_period.min' => 'The Proposed Loan Period field must be at least 2 characters',
+//
+//            'collateral.required' => 'The Collateral Details field is required',
+//            'collateral.min' => 'The Collateral Details field must be at least 5 charaters',
+//
+//            'wname.required' => 'The Witness\' Name field is required',
+//            'wname.min' => 'The Witness\' Name field must be at least 2 characters',
+//
+//            'wemployer.required' => 'The Witness\' Employer field is required',
+//            'wemployer.min' => 'The Witness\' Employer field must be at least 2 character',
+//
+//            'wtel.required' => 'The Witness\' Telephone field is required',
+//            'wtel.min' => 'The Witness\' Telephone must be at least 10 characters'
+//        ];
+//
+//        $this->validate($request, $rules, $messages);
 
         if ($inputs['agree'] == 1) {
 //            return $request->all();
