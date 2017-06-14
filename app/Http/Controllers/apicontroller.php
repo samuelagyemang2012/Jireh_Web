e19cef69698493d212636be4450e5933bdb7be95
samuelagyemang2012 <khermz2012@gmail.com>
2017-06-14 14:53:19 +0100

    api fix

--- app/Http/Controllers/apicontroller.php
+++ app/Http/Controllers/apicontroller.php
@@ -52,59 +52,53 @@ class apicontroller extends Controller
 
         $inputs = $request->all();
 
-        if ($inputs['agree'] == 1) {
+        $uniques = $user->get_uniques($inputs['email'], $inputs['social_security']);
 
-            $uniques = $user->get_uniques($inputs['email'], $inputs['social_security']);
+        if ($uniques == 0) {
 
-            if ($uniques == 0) {
-
-                $dob = preg_replace('/\s+/', '-', $inputs['date_of_birth']);
+            $dob = preg_replace('/\s+/', '-', $inputs['date_of_birth']);
 //                $d = $dob[2]+"-"+$dob[1]+"-"+$dob[0];
 
-                $npass = bcrypt($inputs['password']);
+            $npass = bcrypt($inputs['password']);
 
-                $today = date("l jS \of F Y h:i:s A");
+            $today = date("l jS \of F Y h:i:s A");
 
-                $user->insert($inputs['surname'], $inputs['firstname'], $inputs['othernames'], $npass, $inputs['email'], "test", $today);
-                $spouse->insert($inputs['email'], $inputs['spousename'], $inputs['saddress'], $inputs['stel']);
-                $employer->insert($inputs['email'], $inputs['employer_name'], $inputs['employer_address']);
-                $client->insert($inputs['email'], $inputs['title'], $inputs['gender'], $inputs['num_children'], $inputs['residential_address'], $inputs['mailing_address'], $inputs['telephone_mobile'], $inputs['telephone_official'], $dob, $inputs['occupation'], $inputs['position'], $inputs['nationality'], $inputs['numyears'], $inputs['marital_status'], $inputs['source_of_funds'], $inputs['monthly_income'], $inputs['identification'], $inputs['identification_number'], $inputs['issuedate'], $inputs['expirydate'], $inputs['literacy'], $inputs['hometown'], $inputs['social_security'], $inputs['numhousehold'], $inputs['numdependants'], $inputs['father'], $inputs['mother'], $inputs['kname'], $inputs['kaddress'], $inputs['ktel'], $inputs['krel']);
+            $user->insert($inputs['surname'], $inputs['firstname'], $inputs['othernames'], $npass, $inputs['email'], "test", $today);
+            $spouse->insert($inputs['email'], $inputs['spousename'], $inputs['saddress'], $inputs['stel']);
+            $employer->insert($inputs['email'], $inputs['employer_name'], $inputs['employer_address']);
+            $client->insert($inputs['email'], $inputs['title'], $inputs['gender'], $inputs['num_children'], $inputs['residential_address'], $inputs['mailing_address'], $inputs['telephone_mobile'], $inputs['telephone_official'], $dob, $inputs['occupation'], $inputs['position'], $inputs['nationality'], $inputs['numyears'], $inputs['marital_status'], $inputs['source_of_funds'], $inputs['monthly_income'], $inputs['identification'], $inputs['identification_number'], $inputs['issuedate'], $inputs['expirydate'], $inputs['literacy'], $inputs['hometown'], $inputs['social_security'], $inputs['numhousehold'], $inputs['numdependants'], $inputs['father'], $inputs['mother'], $inputs['kname'], $inputs['kaddress'], $inputs['ktel'], $inputs['krel']);
 
-                $fname = $inputs['firstname'];
-                $sname = $inputs['surname'];
-//
-                $msg = "" . $fname . " " . $sname . " created a new account.";
-                $log->insert($msg, $inputs['email'], 'client');
-//
+            $fname = $inputs['firstname'];
+            $sname = $inputs['surname'];
+
+            $msg = "" . $fname . " " . $sname . " created a new account.";
+            $log->insert($msg, $inputs['email'], 'client');
+
 //            For Mail
-                $body = "You have successfully created an account with Jireh Microfinance Limited.";
-                $sal = " ";
+            $body = "You have successfully created an account with Jireh Microfinance Limited.";
+            $sal = " ";
 
-                $data = ["firstname" => $inputs['firstname'],
-                    "surname" => $inputs['surname'],
-                    "body" => $body,
-                    "salutation" => $sal
-                ];
+            $data = ["firstname" => $inputs['firstname'],
+                "surname" => $inputs['surname'],
+                "body" => $body,
+                "salutation" => $sal
+            ];
 
-                $this->mail($data, $inputs['email'], 'WELCOME TO JIREH MICROFINANCE LTD');
+            $this->mail($data, $inputs['email'], 'WELCOME TO JIREH MICROFINANCE LTD');
 
 //            For SMS
-                $msg2 = "Hello " . $fname . " " . $sname . "," . "\n" . "You have successfully created an account with Jireh Microfinance Limited.";
-                $this->send_sms($inputs['telephone_mobile'], $msg2);
+            $msg2 = "Hello " . $fname . " " . $sname . "," . "\n" . "You have successfully created an account with Jireh Microfinance Limited.";
+            $this->send_sms($inputs['telephone_mobile'], $msg2);
 
-
-                return response()->json([
-                    "code" => 0,
-                    "msg" => "Client created"
-                ]);
-
-            } else {
-
-                return response()->json([
-                    "code" => 9,
-                    "msg" => "Client not created"
-                ]);
-            }
+            return response()->json([
+                "code" => 0,
+                "msg" => "Client created"
+            ]);
+        } else {
+            return response()->json([
+                "code" => 9,
+                "msg" => "Client not created"
+            ]);
         }
     }
 
@@ -153,23 +147,23 @@ class apicontroller extends Controller
         $email = Session::get('memail');
 
 
-            if ($inputs['agree'] == 1) {
+        if ($inputs['agree'] == 1) {
 
-                $loan->insert($inputs['client_email'], $inputs['num_monthly'], $inputs['other_source'], $inputs['bank'], $inputs['salary_date'], $inputs['numloans'], $inputs['total_monthly_payments'], $inputs['name_insti'], $inputs['amount'], $inputs['loan_period'], $inputs['purpose'], $inputs['collateral'], $inputs['cash_service'], $inputs['wname'], $inputs['wemployer'], $inputs['wtel'], $date, 1);
+            $loan->insert($inputs['client_email'], $inputs['num_monthly'], $inputs['other_source'], $inputs['bank'], $inputs['salary_date'], $inputs['numloans'], $inputs['total_monthly_payments'], $inputs['name_insti'], $inputs['amount'], $inputs['loan_period'], $inputs['purpose'], $inputs['collateral'], $inputs['cash_service'], $inputs['wname'], $inputs['wemployer'], $inputs['wtel'], $date, 1);
 
-                $msg = $inputs['client_email'] . " requested for a loan";
-                $log->insert($msg, $inputs['client_email'], "client");
+            $msg = $inputs['client_email'] . " requested for a loan";
+            $log->insert($msg, $inputs['client_email'], "client");
 
-                return response()->json([
-                    "code" => 1,
-                    "msg" => "Loan added"
-                ]);
+            return response()->json([
+                "code" => 1,
+                "msg" => "Loan added"
+            ]);
 
-            } else {
-                return response()->json([
-                    "code" => 0,
-                    "msg" => "Loan not added"
-                ]);
-            }
+        } else {
+            return response()->json([
+                "code" => 0,
+                "msg" => "Loan not added"
+            ]);
         }
+    }
 }
