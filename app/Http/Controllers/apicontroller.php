50b9e86df4006db6b3ae0c64621828e7cec0e594
samuelagyemang2012 <khermz2012@gmail.com>
2017-06-20 16:44:19 +0100

    upload

--- app/Http/Controllers/apicontroller.php
+++ app/Http/Controllers/apicontroller.php
@@ -172,24 +172,65 @@ class apicontroller extends Controller
 
     public function upload(Request $request)
     {
-        $input = $request->all();
+//        $input = $request->all();
+//
+//        $file = $input['testpic'];
+//
+//        if ($file == null) {
+//
+////            $file->move('uploads', $file->getClientOriginalName());
+//
+//            return response()->json([
+//                "data" => "null"
+//            ]);
+//        } else {
+//
+//
+//
+//            return response()->json([
+//                "data" => $file
+//            ]);
+//
+//        }
 
-        $file = $input['testpic'];
+        if (isset($_FILES["file"]["type"])) {
+            $validextensions = array("jpeg", "jpg", "png");
 
-        if ($file == null) {
+            $temporary = explode(".", $_FILES["file"]["name"]);
 
-//            $file->move('uploads', $file->getClientOriginalName());
+            $file_extension = end($temporary);
 
-            return response()->json([
-                "data" => "null"
-            ]);
-        } else {
+            if ((($_FILES["file"]["type"] == "image/png") || ($_FILES["file"]["type"] == "image/jpg") || ($_FILES["file"]["type"] == "image/jpeg")
+                ) && ($_FILES["file"]["size"] < 100000)//Approx. 100kb files can be uploaded.
+                && in_array($file_extension, $validextensions)
+            ) {
 
-            return response()->json([
-                "data" => $file
-            ]);
+                if ($_FILES["file"]["error"] > 0) {
 
+//            echo "Return Code: " . $_FILES["file"]["error"] . "<br/><br/>";
+
+                    return response()->json([
+                        "msg" => "error"
+                    ]);
+
+                } else {
+//            if (file_exists("uploads/" . $_FILES["file"]["name"])) {
+//                echo $_FILES["file"]["name"] . " <span id='invalid'><b>already exists.</b></span> ";
+//            } else {
+                    $sourcePath = $_FILES['file']['tmp_name']; // Storing source path of the file in a variable
+                    $targetPath = "uploads/" . $_FILES['file']['name']; // Target path where file is to be stored
+                    move_uploaded_file($sourcePath, $targetPath); // Moving Uploaded file
+
+                    return response()->json([
+                        "msg" => "done"
+                    ]);
+//                    echo "<span id='success'>Image Uploaded Successfully...!!</span><br/>";
+//                    echo "<br/><b>File Name:</b> " . $_FILES["file"]["name"] . "<br>";
+//                    echo "<b>Type:</b> " . $_FILES["file"]["type"] . "<br>";
+//                    echo "<b>Size:</b> " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+//                    echo "<b>Temp file:</b> " . $_FILES["file"]["tmp_name"] . "<br>";
+                }
+            }
         }
     }
-
 }
